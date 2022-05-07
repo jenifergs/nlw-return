ba893e9b35eaabdd47961e7310f975319bb5ff6f
jenifergs <contato.comercialjenifer@gmail.com>
2022-05-07 13:03:42 -0300

    Finaliza stage 3 do evento

--- main.js
+++ main.js
@@ -15,4 +15,16 @@ function closeMenu () {
 
 }
 
-ScrollReveal().reveal('#home');
+ScrollReveal({
+   origin: 'top',
+   distance: '30px',
+   duration: 700,
+}).reveal(`#home,
+ #home img,
+  #home .stats,
+   #services,
+   #services header,
+   #services .card,
+   #about,
+   #about header,
+   #about .content`);
